61e4096925ea86a52a1f4e3febb25abbc9f957b6
dhruvvohra23 <127841633+dhruvvohra23@users.noreply.github.com>
2023-04-04 12:10:27 +0530

    checking vulnerabilities detection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import json
+import os
 
 app = Flask(__name__)
 
@@ -24,6 +25,7 @@ def open_file():
 
 def dummy_function():
     print("Hello world")
+    os.system('dir')
 
 if __name__ == "__main__":
     app.run(debug=True)
